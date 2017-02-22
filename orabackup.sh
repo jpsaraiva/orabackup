e9e3ce921668940a70d39075f291359585262e9f
jpsaraiva <j3.saraiva@gmail.com>
2017-02-22 15:09:41 +0000

    v1.2
    Removed TNS from orabackup.cfg, get it from tnsnames.ora
    Moved channel
    allocation parameter to config file
    Removed spfile and controlfile
    explicit backup and configured autobackup
    Implemented retry of
    archivelog backup
    Added tmp and log cleanup function
    Added force option
    to ignore running backups
    Catalog operation are now evaluated

--- orabackup.sh
+++ orabackup.sh
@@ -14,10 +14,12 @@
 #  1.0   |         | J.SARAIVA | Creation
 #  1.1   | 20170126| J.SARAIVA | Added tee to execution log
 #  1.2   | 20170222| J.SARAIVA | Removed TNS from orabackup.cfg, get it from tnsnames.ora
-#							   | Moved channel allocation parameter to config file
-#							   | Removed spfile and controlfile explicit backup and configured autobackup
-#							   | Implemented retry of archivelog backup 
-#							   | Added tmp and log cleanup function
+#                              | Moved channel allocation parameter to config file
+#                              | Removed spfile and controlfile explicit backup and configured autobackup
+#                              | Implemented retry of archivelog backup 
+#                              | Added tmp and log cleanup function
+#                              | Added force option to ignore running backups
+#                              | Catalog operation are now evaluated
 #######################################################################################################
 
 SOURCE="${BASH_SOURCE[0]}" #JPS# the script is sourced so this have to be used instead of $0 below
@@ -26,14 +28,14 @@ FILENAME="${PROGNAME%.*}"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 
-REVISION="1.1"
+REVISION="1.2"
 LASTUPDATE="2017-02-22"
 DATA=`date "+%Y%m%d_%H%M%S"`
 
 DEBUG=0
 
 #constants
-RMANARC="BACKUP ARCHIVELOG ALL FILESPERSET 10 FORMAT 'ARCH_%d_%T_%U' TAG 'ARCHIVELOG' DELETE ALL INPUT;"
+RMANARC="BACKUP ARCHIVELOG ALL FILESPERSET 10 FORMAT 'arch_%d_%T_%U' TAG 'archivelog' DELETE ALL INPUT;"
 RMANL0="BACKUP INCREMENTAL LEVEL 0 FILESPERSET 10 FORMAT 'l0inc_%d_%T_%U' TAG 'l0inc' DATABASE;"
 RMANL1="BACKUP INCREMENTAL LEVEL 1 FILESPERSET 10 FORMAT 'l1inc_%d_%T_%U' TAG 'l1inc' DATABASE;"
 RMANSPF="BACKUP SPFILE FORMAT 'spf_%d_%T_%U' TAG 'spfile';"
@@ -51,6 +53,11 @@ Usage:
         -d [DB_NAME]  # Runs the backup for the specified database
         -t [BCK_TYPE] #  l0, l1 , arc
         -c [CATALOG]  #  Resyncs with specified catalog
+        -o [CHNNLDEF] # Overrides default channel allocation
+        -f            # Forces execution ignoring runnning backups
+  --cleanup           # Removes log and tmp files older than 30 days
+  --help              # Displays the help menu
+  --version           # Shows version information
 HEREDOC
 }
 
@@ -58,7 +65,8 @@ print_help() {
   print_revision
 cat <<HEREDOC
 
-  Executes backup on MMK databases using HP DataProtectorâ„¢
+  Executes backup on Oracle databases using:
+	HP DataProtectorâ„¢
 
 HEREDOC
   print_usage
@@ -74,25 +82,23 @@ log() {
   echo `date "+%Y-%m-%d %H:%M:%S"`" $1" | tee -a $LOGFILE
 }
 
-
-construct_channels() {
-RMAN_CH=`grep -i ${CHNNLDEF} ${BASEDIR}/orachannel.cfg | awk -F"|" '{print $2}'`
-for i in {1..${CHNNLNUM}};
-do 
-CHNUMBER=${i}
-echo ${RMAN_CH}
-done;
-}
-
-#todo: action not being validated
 exec_catalog() {
 CATALOG=${_CATALOG}
 #check db conectivity
-CTLG_DB=`grep -i ${CATALOG} ${BASEDIR}/${FILENAME}.cfg | awk -F"|" '{print $1}'`
-CTLG_USER=`grep -i ${CATALOG} ${BASEDIR}/${FILENAME}.cfg | awk -F"|" '{print $2}'`
-CTLG_PASS=`grep -i ${CATALOG} ${BASEDIR}/${FILENAME}.cfg | awk -F"|" '{print $3}'`
-CTLG_PASS=`grep -i ${CATALOG} ${BASEDIR}/${FILENAME}.cfg | awk -F"|" '{print $3}'`
+CTLG_DB=`grep -i ${CATALOG} ${BASEDIR}/orabackup.cfg | awk -F"|" '{print $1}'`
+CTLG_USER=`grep -i ${CATALOG} ${BASEDIR}/orabackup.cfg | awk -F"|" '{print $2}'`
+CTLG_PASS=`grep -i ${CATALOG} ${BASEDIR}/orabackup.cfg | awk -F"|" '{print $3}'`
 CTLG_TNS=`grep -i ${CATALOG} ${BASEDIR}/tnsnames.ora | cut -d= -f2-`
+
+if [[ -z ${CTLG_DB} ]]; then
+  log "ERR : Unable to find catalog database in configuration file"
+  exit 2
+fi
+
+if [[ -z ${CTLG_TNS} ]]; then
+ log "ERR : Unable to find catalog in tnsnames.ora"
+ exit 2
+fi
 
 RMANCONNECT="${CTLG_USER}/${CTLG_PASS}@\"${CTLG_TNS}\""
 
@@ -102,29 +108,76 @@ select trim(dbid) from v\\$database;
 exit;
 !
 `
+
+if [[ ${DBID} == *"ORA-"* ]]; then
+  log "ERR : Error obtaining dbid on target database"
+  exit 2
+fi
+
 RMANREG=`sqlplus -L -s ${RMANCONNECT} <<!
 set pages 0 feed off head off verify off lines 128 echo off term off
 select count(1) from rc_database where dbid=${DBID};
 exit;
 !
 `
+
+if [[ ${DBID} == *"ORA-"* ]]; then
+  log "ERR : Error connecting to catalog database"
+  exit 2
+fi
+
+CATLOG=${BASEDIR}/log/catalog_${DATABASE}_${BACKUP_TYPE}_${DATA}.log
+
 if [[ "${RMANREG}" -eq 0 ]]; then
   log "INF : Database not registered on catalog ${CATALOG}"
-  log "INF : Register in progress ..."
-  rman target ${DBCONNECT} catalog ${RMANCONNECT} 2>&1 >>/dev/null <<!
+  log "INF : Registering and resyncing"
+  rman target ${DBCONNECT} catalog ${RMANCONNECT} log=${CATLOG} 2>&1 >>/dev/null <<!
   register database;
-  exit;
-!
-  log "INF : Registration completed"
-else
-  log "INF : Database registered on catalog ${CATALOG}"
-  log "INF : Resync in progress ..."
-  rman target ${DBCONNECT} catalog ${RMANCONNECT} 2>&1 >>/dev/null <<!
   resync catalog;
   exit;
 !
-  log "INF : Resync completed"
+else
+  log "INF : Database registered on catalog ${CATALOG}"
+  log "INF : Resyncing"
+  rman target ${DBCONNECT} catalog ${RMANCONNECT} log=${CATLOG} 2>&1 >>/dev/null <<!
+  resync catalog;
+  exit;
+!
 fi
+  
+ validate_catalog
+ if [[ $? -ne 0 ]]; then
+   exit 2
+ fi
+}
+
+run_backup() {
+ debug "rman cmdfile=${CMDFILE} log=${RMANLOG}"
+ log "INF : Starting backup ${BACKUP_TYPE} on ${DATABASE}"
+ log "INF : rman log at ${RMANLOG}"
+ rman cmdfile=${CMDFILE} log=${RMANLOG} 2>&1 >>/dev/null 
+}
+
+validate_catalog() {
+ ERRCOUNT=`egrep "ORA-|RMAN-" ${CATLOG} | wc -l`
+ if [[ ERRCOUNT -gt 0 ]]; then
+  log "ERR: Catalog operations failed with ${ERRCOUNT} error(s)"
+  return 2 #will allow for a rerun
+ else
+  log "INF : Catalog operations  successfuly"
+ fi
+}
+
+validate_backup() {
+ #exclude from validation:
+  # RMAN-08120: WARNING: archived log not deleted, not yet applied by standby
+ ERRCOUNT=`egrep "ORA-|RMAN-" ${RMANLOG} | egrep -v "RMAN-08120" | wc -l`
+ if [[ ERRCOUNT -gt 0 ]]; then
+  log "ERR: Backup failed with ${ERRCOUNT} error(s)"
+  return 2 #will allow for a rerun
+ else
+  log "INF : Backup completed successfuly"
+ fi
 }
 
 exec_backup() {
@@ -137,6 +190,7 @@ exec_backup() {
       RMANOP=`printf "%s\n%s\n%s\n%s\n%s\n" "${RMANARC}" "${RMANL1}" "${RMANARC}"`
       ;;
     arc|arch)
+      BACKUP_TYPE=arc
       RMANOP=`printf "%s\n%s\n%s\n" "${RMANARC}"`
       ;;
     *) 
@@ -149,15 +203,7 @@ exec_backup() {
   DBCONNECT="${USERNAME}/${PASSWORD}@\"${TNSNAMES}\""
   CONF_RMAN_RETENTION="CONFIGURE RETENTION POLICY TO RECOVERY WINDOW OF ${RTENTION} DAYS;"
   CONF_RMAN_CTL_AUTOBCK_ON="CONFIGURE CONTROLFILE AUTOBACKUP ON;"
-  
-  #configure channels
-  RMANCHANNELS
-  
-  #RMANCH0="allocate channel 'ch0' type 'sbt_tape' parms 'SBT_LIBRARY=/opt/omni/lib/libob2oracle8_64bit.so,ENV=(OB2BARTYPE=Oracle8,OB2APPNAME=${DATABASE},OB2BARLIST=H_FULL_ORA_RACPRD05_${DATABASE})';"
-  #RMANCH1="allocate channel 'ch1' type 'sbt_tape' parms 'SBT_LIBRARY=/opt/omni/lib/libob2oracle8_64bit.so,ENV=(OB2BARTYPE=Oracle8,OB2APPNAME=${DATABASE},OB2BARLIST=H_FULL_ORA_RACPRD05_${DATABASE})';"
-  #RMANCH2="allocate channel 'ch2' type 'sbt_tape' parms 'SBT_LIBRARY=/opt/omni/lib/libob2oracle8_64bit.so,ENV=(OB2BARTYPE=Oracle8,OB2APPNAME=${DATABASE},OB2BARLIST=H_FULL_ORA_RACPRD05_${DATABASE})';"
-  #RMANCH3="allocate channel 'ch3' type 'sbt_tape' parms 'SBT_LIBRARY=/opt/omni/lib/libob2oracle8_64bit.so,ENV=(OB2BARTYPE=Oracle8,OB2APPNAME=${DATABASE},OB2BARLIST=H_FULL_ORA_RACPRD05_${DATABASE})';"
-  
+    
   #prepare backup 
   RMANLOG=${BASEDIR}/log/rman_${DATABASE}_${BACKUP_TYPE}_${DATA}.log
   CMDFILE=${BASEDIR}/tmp/${DATABASE}_${BACKUP_TYPE}_${DATA}.cmd
@@ -166,10 +212,15 @@ exec_backup() {
   echo ${CONF_RMAN_RETENTION} >> ${CMDFILE}
   echo ${CONF_RMAN_CTL_AUTOBCK_ON} >> ${CMDFILE}
   echo "run {" >> ${CMDFILE}
-  echo ${RMANCH0} >> ${CMDFILE}
-  echo ${RMANCH1} >> ${CMDFILE}
-  echo ${RMANCH2} >> ${CMDFILE}
-  echo ${RMANCH3} >> ${CMDFILE}
+  
+  #configure channels
+  RMAN_CH=`grep -i ${CHNNLDEF} ${BASEDIR}/orachannel.cfg | awk -F"|" '{print $2}'`
+  for (( c=1; c<=${CHNNLNUM}; c++ ))
+   do
+   CHNUMBER=${c}
+   echo "$RMAN_CH" | sed -e 's/_CHNUMBER/'"${CHNUMBER}"'/g' -e 's/_DATABASE/'"${DATABASE}"'/g' >> ${CMDFILE}
+  done;
+  
   echo "${RMANOP}" >> ${CMDFILE}
   echo "}" >> ${CMDFILE}
 
@@ -181,25 +232,39 @@ exec_backup() {
 !
 `
   if [[ "${STILRUN}" -gt  0 ]]; then
-    log "ERR: there is a backup session still running"
-    exit 1
+    if [[ ${FORCERUN} -eq 1 ]]; then
+      log "INF: backup still running but forcing execution"
+    else
+      log "ERR: there is a backup session still running"
+      exit 1
+    fi
   fi
-
+  
   #run backup
-  debug "rman cmdfile=${CMDFILE} log=${RMANLOG}"
-  log "INF : Starting backup ${BACKUP_TYPE} on ${DATABASE}"
-  log "INF : rman log at ${RMANLOG}"
-  rman cmdfile=${CMDFILE} log=${RMANLOG} 2>&1 >>/dev/null 
+  run_backup
   #validate execution
-    #exclude from validation:
-    # RMAN-08120: WARNING: archived log not deleted, not yet applied by standby
-  ERRCOUNT=`egrep "ORA-|RMAN-" ${RMANLOG} | egrep -v "RMAN-08120" | wc -l`
-  if [[ ERRCOUNT -gt 0 ]]; then
-   log "ERR: Backup failed with ${ERRORCOUNT} error(s)"
-   exit 2
-  else
-   log "INF : Backup completed successfuly"
-  fi
+  validate_backup
+  
+  #retry failed arc
+  if [[ ( $? -eq 2 ) ]]; then # if backup failed
+    if [[ ${BACKUP_TYPE} -eq 'arc' ]]; then  # and it is arc, retry
+      log "INF : Retrying"
+      run_backup
+      validate_backup
+      if [[ ( $? -eq 2 ) ]]; then            # if still fail after retrial, error; if not continue
+        log "ERR : Backup failed after retrial"
+        exit 2
+      fi
+    else                                     # if it is not arch, fail immediately
+      exit 2
+    fi    
+  fi    
+}
+
+cleanup() {
+ # remove log and tmp files older than 30 days silently
+ find ${BASEDIR}/tmp -name "*.cmd" -mtime +30 -exec rm {} \;
+ find ${BASEDIR}/log -name "*.log" -mtime +30 -exec rm {} \;
 }
 
 #main(int argc, char *argv[]) #JPS# Start here
@@ -218,11 +283,21 @@ while test -n "$1"; do
     _CATALOG=$2
     shift
     ;;
+   -f)
+    _FORCE=1
+    ;;
+   -o)
+    _notdone=$2
+    shift
+    ;;
    --help|-h)
       print_help
       ;;
    --version|-V|-v)
       print_revision 
+      ;;
+   --cleanup)
+      _CLEANUP=1 
       ;;
     *) 
       print_help
@@ -233,6 +308,10 @@ while test -n "$1"; do
 done
 
 # validation
+if [[ ${_CLEANUP} -eq 1 ]]; then
+  cleanup
+fi
+
 if [[ -z ${_DBNAME} ]]; then
   print_help
   exit
@@ -240,18 +319,40 @@ else
   DB=${_DBNAME}
 fi
 
+if [[ ${_FORCE} -eq 1 ]]; then
+  FORCERUN=1
+fi
+
+if [ ! -d ${BASEDIR}/tmp ]; then
+ mkdir ${BASEDIR}/tmp 
+fi
+
+if [ ! -d ${BASEDIR}/log ]; then
+ mkdir ${BASEDIR}/log
+fi
+
+#set permissions
+chmod 745 ${BASEDIR}/*.sh
+chmod 744 ${BASEDIR}/tnsnames.ora
+
+# Start doing stuff
 LOGFILE=${BASEDIR}/log/exec_${DB}_${DATA}.log
 log "INIT: ${ABSOLUTE_PATH} ${_PARAMS}"
 log "INF : Execution log at ${LOGFILE}"
 
 #check db conectivity
-DATABASE=`grep -i ${DB} ${BASEDIR}/${FILENAME}.cfg | awk -F"|" '{print $1}'`
-USERNAME=`grep -i ${DB} ${BASEDIR}/${FILENAME}.cfg | awk -F"|" '{print $2}'`
-PASSWORD=`grep -i ${DB} ${BASEDIR}/${FILENAME}.cfg | awk -F"|" '{print $3}'`
-RTENTION=`grep -i ${DB} ${BASEDIR}/${FILENAME}.cfg | awk -F"|" '{print $4}'`
-CHNNLDEF=`grep -i ${DB} ${BASEDIR}/${FILENAME}.cfg | awk -F"|" '{print $5}'`
-CHNNLNUM=`grep -i ${DB} ${BASEDIR}/${FILENAME}.cfg | awk -F"|" '{print $6}'`
+DATABASE=`grep -i ${DB} ${BASEDIR}/orabackup.cfg | awk -F"|" '{print $1}'`
+USERNAME=`grep -i ${DB} ${BASEDIR}/orabackup.cfg | awk -F"|" '{print $2}'`
+PASSWORD=`grep -i ${DB} ${BASEDIR}/orabackup.cfg | awk -F"|" '{print $3}'`
+RTENTION=`grep -i ${DB} ${BASEDIR}/orabackup.cfg | awk -F"|" '{print $4}'`
+CHNNLDEF=`grep -i ${DB} ${BASEDIR}/orabackup.cfg | awk -F"|" '{print $5}'`
+CHNNLNUM=`grep -i ${DB} ${BASEDIR}/orabackup.cfg | awk -F"|" '{print $6}'`
 TNSNAMES=`grep -i ${DB} ${BASEDIR}/tnsnames.ora | cut -d= -f2-`
+
+if [[ -z ${USERNAME} || -z ${PASSWORD} || -< ${RETENTION} || -z ${CHNNLDEF} || -z ${CHNNLNUM} || -z ${TNSNAMES} ]]; then
+ log "ERR: unable to get proper configuration for database ${DB}"
+ exit 2
+fi
 
 SQLTEST=`sqlplus -v 2>/dev/null`
 if [[ -z $SQLTEST ]]; then
